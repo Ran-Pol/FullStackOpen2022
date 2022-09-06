4550125977b60877b343b7fc162945bec75d1927
Ran Pol <rnd809@gmail.com>
2022-09-06 17:41:15 -0400

    Refactore the components

--- Part1/courseinfo/src/App.js
+++ Part1/courseinfo/src/App.js
@@ -2,11 +2,7 @@ import { useState } from 'react'
 
 const Display = ({counter}) =><div>{counter}</div>
 
-
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
-
-
-
 
 const App = () => {
   const [ counter, setCounter ] = useState(0)
