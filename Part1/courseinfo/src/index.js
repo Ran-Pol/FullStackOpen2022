a3ecd1521092b6bb89de9d465c2b542b640c8662
Ran Pol <rnd809@gmail.com>
2022-09-06 16:36:50 -0400

    Add state to our application's App component with the help of React's state hook.

--- Part1/courseinfo/src/index.js
+++ Part1/courseinfo/src/index.js
@@ -3,15 +3,4 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-let counter = 1
-
-const refresh = () => {
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <App counter={counter} />
-  )
-}
-refresh()
-// setInterval(() => {
-//   refresh()
-//   counter += 1
-// }, 1000)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
