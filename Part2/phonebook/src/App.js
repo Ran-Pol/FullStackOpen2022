34659bd35328e3858d39d0d34ea746c20493501d
Ran Pol <rnd809@gmail.com>
2022-09-11 18:53:48 -0400

    2.8: The Phonebook Step3: Expand funcionality to allow  users to add a phone numbers to the phone book.

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", phone:"1-800-234-3333"}]);
   const [newName, setNewName] = useState("");
+  const [newPhone, setNewPhone] = useState("");
 
   const addContactHandeler = (event) => {
     event.preventDefault();
@@ -17,10 +18,12 @@ const App = () => {
     const newContact = [
       {
         name: newName.trim(),
+        phone: newPhone
       },
     ];
     setPersons(persons.concat(newContact));
     setNewName("");
+    setNewPhone("");
   };
 
   return (
@@ -28,16 +31,20 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addContactHandeler}>
         <div>
-          name:{" "}
-          <input onChange={(e) => setNewName(e.target.value)} value={newName} />
+          Name:{" "}
+          <input onChange={(e) => setNewName(e.target.value)} value={newName} placeholder="Enter fullname"/>
+        </div>
+        <div>
+          Phone:{" "}
+          <input onChange={(e) => setNewPhone(e.target.value)} value={newPhone} placeholder="Enter phone number"/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person, i) => (
-        <p key={i}>{person.name}</p>
+      {persons.map(({name,phone}, i) => (
+        <p key={i}>{name} {phone}</p>
       ))}
     </div>
   );
