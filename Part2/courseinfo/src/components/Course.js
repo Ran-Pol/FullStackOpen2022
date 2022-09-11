c43cdbadcaf1f027fc047f655dbec7b2b9eefa68
Ran Pol <rnd809@gmail.com>
2022-09-11 01:25:04 -0400

    2.4: Course information step9: Extend the application to allow for an arbitrary number of courses

--- Part2/courseinfo/src/components/Course.js
+++ Part2/courseinfo/src/components/Course.js
@@ -1,20 +1,19 @@
 import Content from "./Content";
 import Header from "./Header";
 
-const Course = ({ course }) => {
-  const { name, parts } = course;
-  return (
-    <div>
-      <Header name={name} />
-      <Content parts={parts} />
-    </div>
-  );
+const Course = ({ courses }) => {
+const allCourses = courses.map((item, i) =>{
+    const {name, parts} = item;
+   return(
+    <div key={i}>
+    <Header name={name} />
+    <Content parts={parts} />
+  </div>
+   )}
+)
+
+return allCourses
+
 };
 
 export default Course;
-// parts: [
-//     {
-//       name: 'Fundamentals of React',
-//       exercises: 10,
-//       id: 1
-//     },
