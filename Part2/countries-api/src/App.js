59aaa60fbe2182774f8bb0b09ba3a0bf94bda4a4
Ran Pol <rnd809@gmail.com>
2022-09-13 06:01:41 -0400

    Setup the data structure that is needed to complete a country's profile

--- Part2/countries-api/src/App.js
+++ Part2/countries-api/src/App.js
@@ -6,31 +6,29 @@ import ListOfCountries from "./components/ListOfCountries";
 
 function App() {
   const [countries_raw, setCountriesRaw] = useState([]);
-  const [filterName, setFilterName] = useState('');
+  const [filterName, setFilterName] = useState("");
 
   const hook = () => {
     axios.get("https://restcountries.com/v3.1/all").then((response) => {
       setCountriesRaw(response.data);
-    })
+    });
   };
   useEffect(hook, []);
-
 
   const applyFilter = () => {
     const newWord = filterName.trim().toLowerCase();
 
-    const newFilterList = countries_raw.filter(({name:{common}}) =>
+    const newFilterList = countries_raw.filter(({ name: { common } }) =>
       common.trim().toLowerCase().includes(newWord)
     );
     return newFilterList;
   };
 
-
   return (
     <div>
       <Filter setFilterName={setFilterName} />
       <h2>Lis of countries</h2>
-      <ListOfCountries applyFilter={applyFilter} filterName={filterName}/>
+      <ListOfCountries applyFilter={applyFilter} filterName={filterName} />
     </div>
   );
 }
--- Part2/countries-api/src/components/ListOfCountries.js
+++ Part2/countries-api/src/components/ListOfCountries.js
@@ -1,15 +1,30 @@
 const ListOfCountries = ({ applyFilter, filterName }) => {
-  const listResults = applyFilter().map(({name:{common}}, i) => (
+  const filteredCountries = applyFilter();
+  const listResults = filteredCountries.map(({ name: { common } }, i) => (
     <p key={i}>{common}</p>
   ));
   const lteTen_gTZero =
     listResults.length > 0 && listResults.length <= 10
-    // If the first test passes succesfully then we will be send an array.
-      ? listResults 
-      : (listResults.length > 10 && filterName.trim().length)
+      ? // If the first test passes succesfully then we will be send an array.
+        listResults
+      : listResults.length > 10 && filterName.trim().length
       ? "Too many matches, specify another filter"
-      : "No results..."
-console.log(typeof lteTen_gTZero,lteTen_gTZero)
+      : "No results...";
+  if (filteredCountries.length === 1) {
+    const {
+      name: { common: countryName },
+      capital: [capital],
+      area,
+      languages,
+      flags: { svg: flag },
+    } = filteredCountries[0];
+    console.log(countryName);
+    console.log(capital);
+    console.log(area);
+    console.log(JSON.stringify(languages));
+    console.log(flag);
+  }
+  // const singleCountry = lteTen_gTZero.lenght === 1?
   return <div>{lteTen_gTZero}</div>;
 };
 
