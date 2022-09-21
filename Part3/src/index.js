52d92e2bd9f8aa50ce8e9fff2c1c7880faf2657c
Ran Pol <rnd809@gmail.com>
2022-09-21 07:54:03 -0400

    3.8*: Phonebook backend step8: Configure morgan so that it also shows the data sent in HTTP POST requests.

--- Part3/src/index.js
+++ Part3/src/index.js
@@ -1,11 +1,42 @@
 const express = require("express");
+const { json } = require("express/lib/response");
 const morgan = require("morgan");
+const uuid = require("uuid");
+
+morgan.token("bodyData", function getId(req) {
+  return req.bodyData;
+});
+
+function assignJsonBody(req, res, next) {
+  req.bodyData = JSON.stringify(req.body);
+  next();
+}
 
 const app = express();
 
 app.use(express.json());
-app.use(morgan("tiny"));
 
+app.use(assignJsonBody);
+
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :bodyData"
+  )
+);
+// app.use(morgan("tiny"));
+// app.use(
+//   morgan(function (tokens, req, res) {
+//     return [
+//       tokens.method(req, res),
+//       tokens.url(req, res),
+//       tokens.status(req, res),
+//       tokens.res(req, res, "content-length"),
+//       "-",
+//       tokens["response-time"](req, res),
+//       "ms",
+//     ].join(" ");
+//   })
+// );
 // const requestLogger = (request, response, next) => {
 //   console.log("Method:", request.method);
 //   console.log("Path:  ", request.path);
