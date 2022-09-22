b711ddc00f42d9499aa13b9c6675ace945c09ea2
Ran Pol <rnd809@gmail.com>
2022-09-22 15:41:27 -0400

    NotesApp: Moving error handling into middleware

--- Part3/notesBAckEnd/index.js
+++ Part3/notesBAckEnd/index.js
@@ -55,7 +55,7 @@ app.delete("/api/notes/:id", (request, response) => {
   response.status(204).end();
 });
 
-app.get("/api/notes/:id", (request, response) => {
+app.get("/api/notes/:id", (request, response, next) => {
   Note.findById(request.params.id)
     .then((note) => {
       if (note) {
@@ -64,10 +64,7 @@ app.get("/api/notes/:id", (request, response) => {
         response.status(404).end();
       }
     })
-    .catch((error) => {
-      console.log(error);
-      response.status(400).send({ error: "malformatted id" });
-    });
+    .catch((error) => next(error));
 });
 
 const unknownEndpoint = (request, response) => {
@@ -75,6 +72,19 @@ const unknownEndpoint = (request, response) => {
 };
 
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+// this has to be the last loaded middleware.
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
