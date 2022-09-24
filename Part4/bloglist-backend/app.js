493b8b1efa7c56708c8d7811d01030591dcaa185
Ran Pol <rnd809@gmail.com>
2022-09-24 04:55:06 -0400

    BlogApp: Install eslint to have a standard syntax configuration to follow

--- Part4/bloglist-backend/app.js
+++ Part4/bloglist-backend/app.js
@@ -1,28 +1,29 @@
-const config = require("./utils/config");
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const blogsRouter = require("./controllers/blog");
-const middleware = require("./utils/middleware");
-const { info, error } = require("./utils/logger");
-const mongoose = require("mongoose");
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blog')
+const middleware = require('./utils/middleware')
+// eslint-disable-next-line no-unused-vars
+const { info, error } = require('./utils/logger')
+const mongoose = require('mongoose')
 
 mongoose
   .connect(config.MONGODB_URI)
   .then(() => {
-    info("connected to MongoDB");
+    info('connected to MongoDB')
   })
   .catch((error) => {
-    error("error connecting to MongoDB:", error.message);
-  });
+    error('error connecting to MongoDB:', error.message)
+  })
 
-app.use(cors());
-app.use(express.json());
-app.use(middleware.requestLogger);
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
 
-app.use("/api/blogs", blogsRouter);
+app.use('/api/blogs', blogsRouter)
 
-app.use(middleware.unknownEndpoint);
-app.use(middleware.errorHandler);
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
-module.exports = app;
+module.exports = app
--- Part4/bloglist-backend/controllers/blog.js
+++ Part4/bloglist-backend/controllers/blog.js
@@ -1,18 +1,18 @@
-const blogsRouter = require("express").Router();
-const Blog = require("../models/blog");
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
 
-blogsRouter.get("/", (req, res) => {
+blogsRouter.get('/', (req, res) => {
   Blog.find({}).then((blogs) => {
-    res.json(blogs);
-  });
-});
+    res.json(blogs)
+  })
+})
 
-blogsRouter.post("/", (req, res) => {
-  const blog = new Blog(req.body);
+blogsRouter.post('/', (req, res) => {
+  const blog = new Blog(req.body)
 
   blog.save().then((result) => {
-    res.status(201).json(result);
-  });
-});
+    res.status(201).json(result)
+  })
+})
 
-module.exports = blogsRouter;
+module.exports = blogsRouter
