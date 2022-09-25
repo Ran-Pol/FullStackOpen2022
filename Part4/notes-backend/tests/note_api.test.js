a79f3c6d1154c0e394832d95f082b03f2a40522b
Ran Pol <rnd809@gmail.com>
2022-09-24 22:02:26 -0400

    NotesApp: Initializing the database before tests. In order to make our tests more robust, we have to reset the database and generate the needed test data in a controlled manner before we run the tests.

--- Part4/notes-backend/tests/note_api.test.js
+++ Part4/notes-backend/tests/note_api.test.js
@@ -1,9 +1,31 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+const Note = require('../models/note')
 
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    date: new Date(),
+    important: false,
+  },
+  {
+    content: 'Browser can execute only Javascript',
+    date: new Date(),
+    important: true,
+  },
+]
+
+beforeEach(async () => {
+  await Note.deleteMany({})
+  let noteObject = new Note(initialNotes[0])
+  await noteObject.save()
+  noteObject = new Note(initialNotes[1])
+  await noteObject.save()
+})
+
+// Test 1
 test('notes are returned as json', async () => {
   await api
     .get('/api/notes')
@@ -11,18 +33,23 @@ test('notes are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 }, 100000)
 
-test('there are two notes', async () => {
-  const response = await api.get('/api/notes')
+// Test 2
+test('all notes are returned', async () => {
+    const response = await api.get('/api/notes')
+  
+    expect(response.body).toHaveLength(initialNotes.length)
+  })
+  
 
-  expect(response.body).toHaveLength(2)
-})
-
-test('the first note is about HTTP methods', async () => {
-  const response = await api.get('/api/notes')
-
-  expect(response.body[0].content).toBe('HTML is easy')
-})
-
+// Test 3
+test('a specific note is within the returned notes', async () => {
+    const response = await api.get('/api/notes')
+  
+    const contents = response.body.map(r => r.content)
+    expect(contents).toContain(
+      'Browser can execute only Javascript'
+    )
+  })
 afterAll(() => {
   mongoose.connection.close()
 })
