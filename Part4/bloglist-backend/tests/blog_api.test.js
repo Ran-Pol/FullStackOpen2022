9ab8719afad955c1a5b0fbd2710b89a8035b522f
Ran Pol <rnd809@gmail.com>
2022-10-02 06:34:39 -0400

    4.16*: bloglist expansion, step4: Implement tests which check that invalid users are not created and invalid add user operation returns a suitable status code and error message.

--- Part4/bloglist-backend/tests/blog_api.test.js
+++ Part4/bloglist-backend/tests/blog_api.test.js
@@ -5,6 +5,7 @@ const app = require('../app')
 const api = supertest(app)
 
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 beforeEach(async () => {
   await Blog.deleteMany({})
@@ -108,6 +109,65 @@ describe('updating of a blog post', () => {
     const contents = blogssAtEnd.map((r) => r.likes)
 
     expect(contents).toContain(16725422)
+  })
+})
+
+// Api Tests For Users
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const newUser = new User({
+      username: 'root',
+      name: 'Jason',
+      password: await User.encryptPassword('123456'),
+    })
+
+    await newUser.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map((u) => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
+
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('username must be unique')
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toEqual(usersAtStart)
   })
 })
 
--- Part4/bloglist-backend/tests/test_helper.js
+++ Part4/bloglist-backend/tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialBlogs = [
   {
@@ -26,7 +27,14 @@ const blogsInDb = async () => {
   return blogs.map((blog) => blog.toJSON())
 }
 
+// Users Test Helpers Functions
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map((u) => u.toJSON())
+}
+
 module.exports = {
   initialBlogs,
   blogsInDb,
+  usersInDb,
 }
