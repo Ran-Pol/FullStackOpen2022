e6f490e82271eebf619a2bd489068d50b467f807
Ran Pol <rnd809@gmail.com>
2022-10-16 00:23:18 -0400

    5.2: bloglist frontend, step2

--- Part5/bloglist-frontend/src/App.js
+++ Part5/bloglist-frontend/src/App.js
@@ -14,8 +14,18 @@ function App() {
     password: '',
   })
 
+  // useEffect to fetch all blog post data and setting to state variable
   React.useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs))
+  }, [])
+
+  // Check if there is a user in the localstorage
+  React.useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
   }, [])
 
   const listOfBlogs = blogs.map((blog) => <Blog key={blog.id} blog={blog} />)
@@ -24,11 +34,16 @@ function App() {
     const { name, value } = e.target
     setUserLogin((prev) => ({ ...prev, [name]: value }))
   }
+  const userLogOut = () => {
+    window.localStorage.removeItem('loggedBlogUser')
+    setUser((prev) => !prev)
+  }
 
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
       const user = await loginService(userLogin)
+      window.localStorage.setItem('loggedBlogUser', JSON.stringify(user))
       setUser(user)
       setUserLogin({
         username: '',
@@ -47,7 +62,10 @@ function App() {
       {user ? (
         <>
           <h1>Blogs</h1>
-          <h2>{user.name} logged in</h2>
+          <div>
+            <h2>{user.name} logged in</h2>
+            <button onClick={userLogOut}>Logout</button>
+          </div>
           {listOfBlogs}
         </>
       ) : (
