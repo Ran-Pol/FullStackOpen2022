70d9323529dbf98a025b76341effd6586986e7ca
Ran Pol <rnd809@gmail.com>
2022-10-18 16:31:44 -0400

    5.7* Blog list frontend, step7: Let's add a button to each blog, which controls whether all of the details about the blog are shown or not.

--- Part5/bloglist-frontend/src/components/Blog.js
+++ Part5/bloglist-frontend/src/components/Blog.js
@@ -1,11 +1,44 @@
 import React from 'react'
 
 const Blog = ({ blog }) => {
+  const [viewAll, setViewAll] = React.useState(true)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
   return (
-    <div>
-      {blog.title} {blog.author}
-    </div>
+    <>
+      {viewAll ? (
+        <div style={blogStyle}>
+          <p>
+            {blog.title} {blog.author}
+            <button onClick={() => setViewAll((prev) => !prev)}>view</button>
+          </p>
+        </div>
+      ) : (
+        <div style={blogStyle}>
+          <p>
+            {blog.title} {blog.author}{' '}
+            <button onClick={() => setViewAll((prev) => !prev)}>hide</button>
+          </p>
+
+          <p>{blog.url}</p>
+          <p>{blog.likes}</p>
+          <p>{blog.user.name}</p>
+        </div>
+      )}
+    </>
   )
 }
 
 export default Blog
+
+// author
+// id
+// likes
+// title
+// url
+// user {name, username}
