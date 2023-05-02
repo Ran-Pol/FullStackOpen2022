72160b1676b310e9c0569bb76f5c8be9f061cd8a
Ran Pol <rnd809@gmail.com>
2023-05-02 16:06:39 -0400

    implement the filters.

--- Part6/A_Flux_Redux/src/index.js
+++ Part6/A_Flux_Redux/src/index.js
@@ -8,19 +8,12 @@ import './index.css'
 import noteReducer from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
 
-import { createNote } from './reducers/noteReducer'
-import { filterChange } from './reducers/filterReducer'
-
 const reducer = combineReducers({
   notes: noteReducer,
   filter: filterReducer,
 })
 
 const store = createStore(reducer)
-
-store.subscribe(() => console.log(store.getState()))
-store.dispatch(filterChange('IMPORTANT'))
-store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
