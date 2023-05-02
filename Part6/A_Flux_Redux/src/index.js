93cd4469548b2b5826352aaed103d59329217277
Ran Pol <rnd809@gmail.com>
2023-05-02 10:59:39 -0400

    Import the noteReducer to index.

--- Part6/A_Flux_Redux/src/index.js
+++ Part6/A_Flux_Redux/src/index.js
@@ -5,22 +5,7 @@ import './index.css'
 
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-
-const noteReducer = (state = [], action) => {
-  if (action.type === 'NEW_NOTE') {
-    return state.concat(action.payload)
-  } else if (action.type === 'TOGGLE_IMPORTANCE') {
-    const id = action.payload.id
-    const noteToChange = state.find((n) => n.id === id)
-    const changedNote = {
-      ...noteToChange,
-      important: !noteToChange.important,
-    }
-    return state.map((note) => (note.id !== id ? note : changedNote))
-  }
-
-  return state
-}
+import noteReducer from './reducers/noteReducer'
 
 const store = createStore(noteReducer)
 
