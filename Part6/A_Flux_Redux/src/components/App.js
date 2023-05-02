a58c9e1b7e3645b6f0cd185822862845e5738f0f
Ran Pol <rnd809@gmail.com>
2023-05-02 15:49:07 -0400

    Extract the visibility filter into its own.

--- Part6/A_Flux_Redux/src/components/App.js
+++ Part6/A_Flux_Redux/src/components/App.js
@@ -1,35 +1,13 @@
 import React from 'react'
 import NewNote from './NewNote'
 import Notes from './Notes'
+import VisibilityFilter from './VisibilityFilter'
 
 const App = () => {
-  const filterSelected = (value) => {
-    console.log(value)
-  }
-
   return (
     <div>
       <NewNote />
-      <div>
-        all
-        <input
-          type="radio"
-          name="filter"
-          onChange={() => filterSelected('ALL')}
-        />{' '}
-        important
-        <input
-          type="radio"
-          name="filter"
-          onChange={() => filterSelected('IMPORTANT')}
-        />{' '}
-        nonimportant
-        <input
-          type="radio"
-          name="filter"
-          onChange={() => filterSelected('NONIMPORTANT')}
-        />
-      </div>
+      <VisibilityFilter />
       <Notes />
     </div>
   )
