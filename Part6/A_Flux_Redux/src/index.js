468b5551f816a8bad30ac5ef8f64b1c4091958ae
Ran Pol <rnd809@gmail.com>
2023-05-02 15:27:51 -0400

    Combined reducers.

--- Part6/A_Flux_Redux/src/index.js
+++ Part6/A_Flux_Redux/src/index.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
 import App from './components/App'
 import './index.css'
 
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
 import noteReducer from './reducers/noteReducer'
+import filterReducer from './reducers/filterReducer'
 
-const store = createStore(noteReducer)
+import { createNote } from './reducers/noteReducer'
+import { filterChange } from './reducers/filterReducer'
+
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer,
+})
+
+const store = createStore(reducer)
+
+store.subscribe(() => console.log(store.getState()))
+store.dispatch(filterChange('IMPORTANT'))
+store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
