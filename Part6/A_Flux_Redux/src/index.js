b400a749fc565822b42c8f892fdb3a38ed21e56c
Ran Pol <rnd809@gmail.com>
2023-05-02 02:27:01 -0400

    Add the add notes functionality and the toggle dispatch.

--- Part6/A_Flux_Redux/src/index.js
+++ Part6/A_Flux_Redux/src/index.js
@@ -6,6 +6,14 @@ import { createStore } from 'redux'
 const noteReducer = (state = [], action) => {
   if (action.type === 'NEW_NOTE') {
     return state.concat(action.payload)
+  } else if (action.type === 'TOGGLE_IMPORTANCE') {
+    const id = action.payload.id
+    const noteToChange = state.find((n) => n.id === id)
+    const changedNote = {
+      ...noteToChange,
+      important: !noteToChange.important,
+    }
+    return state.map((note) => (note.id !== id ? note : changedNote))
   }
 
   return state
@@ -33,12 +41,39 @@ store.dispatch({
   },
 })
 
+const generateId = () => Number((Math.random() * 1000000).toFixed(0))
+
 const App = () => {
+  const addNote = (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    store.dispatch({
+      type: 'NEW_NOTE',
+      payload: {
+        content,
+        important: false,
+        id: generateId(),
+      },
+    })
+  }
+
+  const toggleImportance = (id) => {
+    store.dispatch({
+      type: 'TOGGLE_IMPORTANCE',
+      payload: { id },
+    })
+  }
+
   return (
     <div>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
       <ul>
         {store.getState().map((note) => (
-          <li key={note.id}>
+          <li key={note.id} onClick={() => toggleImportance(note.id)}>
             {note.content} <strong>{note.important ? 'important' : ''}</strong>
           </li>
         ))}
