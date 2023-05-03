529bb646086b0d4be6cb6d80bbb705bb410b3781
Ran Pol <rnd809@gmail.com>
2023-05-02 20:25:35 -0400

    Install  Redux Toolkit in our application

--- Part6/A_Flux_Redux/src/index.js
+++ Part6/A_Flux_Redux/src/index.js
@@ -1,29 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
 import App from './components/App'
 import './index.css'
 
 import noteReducer from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
 
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer,
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer,
+  },
 })
 
-const store = createStore(reducer)
+console.log(store.getState())
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-const renderApp = () => {
-  root.render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  )
-}
-
-renderApp()
-store.subscribe(renderApp)
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
