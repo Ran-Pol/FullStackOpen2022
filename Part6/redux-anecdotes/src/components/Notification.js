456f34550864e0b2f18abc5f70c71814d6598866
Ran Pol <rnd809@gmail.com>
2023-05-03 03:16:16 -0400

    feat: 6.13 Better anecdotes, step11

--- Part6/redux-anecdotes/src/components/Notification.js
+++ Part6/redux-anecdotes/src/components/Notification.js
@@ -1,13 +1,27 @@
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { removeNotification } from '../reducers/notificationReducer'
 
 const Notification = () => {
   const notification = useSelector((state) => state.notification)
+  const dispatch = useDispatch()
+
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1,
   }
-  return <div style={style}>{notification}</div>
+
+  if (notification.timeout > 0) {
+    setTimeout(() => {
+      dispatch(removeNotification())
+    }, notification.timeout * 1000)
+  }
+
+  if (notification.message === '') {
+    return null
+  }
+
+  return <div style={style}>{notification.message}</div>
 }
 
 export default Notification
