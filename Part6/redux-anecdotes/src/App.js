0af3a61729cedc22cba2f4a6a0e872aef6598985
Ran Pol <rnd809@gmail.com>
2023-05-03 20:05:07 -0400

    feat: Fetch initial notes from backend.

--- Part6/redux-anecdotes/src/App.js
+++ Part6/redux-anecdotes/src/App.js
@@ -1,9 +1,20 @@
+import { useEffect } from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import anecdoteServices from './services/anecdotes'
+import { setNotes } from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    console.log('Inside useEffect')
+    anecdoteServices.getAll().then((anecdotes) => dispatch(setNotes(anecdotes)))
+  }, [dispatch])
+
   return (
     <div>
       <h2>Anecdotes</h2>
