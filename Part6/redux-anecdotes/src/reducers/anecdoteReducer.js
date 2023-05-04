ab0bcfd4ba99261624f734e51b31d8df447d38af
Ran Pol <rnd809@gmail.com>
2023-05-03 20:04:17 -0400

    Add setNotes reducer to the noteSlice.

--- Part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ Part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,39 +1,20 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { setNotification } from './notificationReducer'
 
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-]
-
-const getId = () => (100000 * Math.random()).toFixed(0)
-const asObject = (anecdote) => ({
-  content: anecdote,
-  id: getId(),
-  votes: 0,
-})
-const initialState = anecdotesAtStart.map(asObject)
-
 const noteSlice = createSlice({
   name: 'notes',
-  initialState,
+  initialState: [],
   reducers: {
     createNote: (state, action) => {
-      const content = action.payload
-      state.push({
-        content,
-        id: getId(),
-        votes: 0,
-      })
+      state.push(action.payload)
     },
     voteFor: (state, action) => {
       const id = action.payload
       const noteToChange = state.find((n) => n.id === id)
       noteToChange.votes += 1
+    },
+    setNotes(_, action) {
+      return action.payload
     },
   },
 })
@@ -52,4 +33,5 @@ export const voteFor = (id) => {
   }
 }
 
+export const { setNotes } = noteSlice.actions
 export default noteSlice.reducer
