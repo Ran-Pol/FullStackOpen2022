7dbf49e4746a939fe618956cbc03c12c1af7d363
Ran Pol <rnd809@gmail.com>
2023-05-03 23:36:32 -0400

    Refactor: Move the store creation into its own file.

--- Part6/A_Flux_Redux/src/index.js
+++ Part6/A_Flux_Redux/src/index.js
@@ -1,19 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { configureStore } from '@reduxjs/toolkit'
 import App from './components/App'
+import store from './store'
 import './index.css'
-
-import noteReducer from './reducers/noteReducer'
-import filterReducer from './reducers/filterReducer'
-
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer,
-  },
-})
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
