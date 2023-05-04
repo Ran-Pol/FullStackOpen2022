dc809cbcd7330656fc82c7c18aafa69ec277b1c2
Ran Pol <rnd809@gmail.com>
2023-05-04 13:23:51 -0400

    Install react-query and update index/app files.

--- Part6/ReactQuery/src/App.js
+++ Part6/ReactQuery/src/App.js
@@ -1,3 +1,6 @@
+import { useQuery } from 'react-query'
+import { getNotes } from './requests'
+
 const App = () => {
   const addNote = async (event) => {
     event.preventDefault()
@@ -10,21 +13,28 @@ const App = () => {
     console.log('toggle importance of', note.id)
   }
 
-  const notes = []
+  const result = useQuery('notes', getNotes)
+  console.log(result)
 
-  return(
+  if (result.isLoading) {
+    return <div>loading data...</div>
+  }
+
+  const notes = result.data
+
+  return (
     <div>
       <h2>Notes app</h2>
       <form onSubmit={addNote}>
         <input name="note" />
         <button type="submit">add</button>
       </form>
-      {notes.map(note =>
+      {notes.map((note) => (
         <li key={note.id} onClick={() => toggleImportance(note)}>
-          {note.content} 
+          {note.content}
           <strong> {note.important ? 'important' : ''}</strong>
         </li>
-      )}
+      ))}
     </div>
   )
 }
