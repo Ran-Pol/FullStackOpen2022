77882bd527ac0113497abd34f1de419d95138328
Ran Pol <rnd809@gmail.com>
2023-05-04 18:52:15 -0400

    Implement adding new anecdotes to the server using React Query.

--- Part6/query-anecdotes-main/src/components/AnecdoteForm.js
+++ Part6/query-anecdotes-main/src/components/AnecdoteForm.js
@@ -1,17 +1,21 @@
+import { useMutation } from 'react-query'
+import { createAnecdote } from '../requests'
+
 const AnecdoteForm = () => {
+  const newAnecdoteMutation = useMutation(createAnecdote)
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+    newAnecdoteMutation.mutate({ content, votes: 0 })
+  }
 
   return (
     <div>
       <h3>create new</h3>
       <form onSubmit={onCreate}>
-        <input name='anecdote' />
+        <input name="anecdote" />
         <button type="submit">create</button>
       </form>
     </div>
